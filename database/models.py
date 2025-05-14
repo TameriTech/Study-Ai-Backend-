acfc15b0f820662efe87dc93b61c222be7ced6bf
Temban Blaise Ayim <tembanblaise12@gmail.com>
2025-05-14 10:27:16 +0100

    import db

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from database.db import Base
+from db import Base
 from sqlalchemy import Integer, Column, String, Date, Numeric
 from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey, Enum, JSON
 from sqlalchemy.orm import relationship
